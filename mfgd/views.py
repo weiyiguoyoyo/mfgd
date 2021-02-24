5eed813650d63d14f35c54926c99653168b614f4
Mate Kukri <kukri.mate@gmail.com>
2021-02-24 15:18:51 +0000

    Add web POC

--- mfgd/views.py
+++ mfgd/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
 from pygit2 import *
 
+def format_author(commit):
+    return "%s <%s>" %(commit.author.name, commit.author.email)
+
+def print_tree(tree,indent=0):
+    r = ""
+    for obj in tree:
+        r += "  " * indent + obj.name + "\n"
+        if obj.type_str == "tree":
+            r += print_tree(obj, indent+1)
+    return r
+
 def index(request):
-	return HttpResponse("Test")
+    repo = Repository(".git")
+    branch = next(iter(repo.branches.local))
+    branch_ref = repo.references["refs/heads/%s" %branch]
+
+    r = ""
+    for commit in repo.walk(branch_ref.target, GIT_SORT_TOPOLOGICAL):
+        r += "%s\n%s\n%s\n" %(commit.tree_id, format_author(commit), commit.message)
+        print_tree(commit.tree)
+
+    return HttpResponse(r, content_type='text/plain')
