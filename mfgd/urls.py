dba28ce123988a3d2eecda7c760faa1ab12dd8b7
Paul Bekaert <2517073B@student.gla.ac.uk>
2021-03-11 13:55:48 +0000

    Added register

--- mfgd/urls.py
+++ mfgd/urls.py
@@ -18,9 +18,10 @@ from django.urls import path, re_path
 from mfgd_app import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name='index'),
     re_path(r"view/(?P<oid>\w+)/(?P<path>\S*)", views.view, name="view"),
     path("admin/", admin.site.urls),
     path('logout/', views.user_logout, name='logout'),
     path('login/', views.user_login, name='login'),
+    path('register/', views.user_register, name='register'),
 ]
