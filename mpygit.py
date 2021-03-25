28e49928d6b6f2bb34fec4d8640f0820a045ab62
Mate Kukri <kukri.mate@gmail.com>
2021-03-25 12:25:02 +0000

    Add GPL header to mpygit

--- mpygit.py
+++ mpygit.py
@@ -4,7 +4,22 @@
 # This is really outside the scope of the WAD2 project, but I decided to write
 # a replacement for pygit2 as it was not flexible enough for our requirements
 #
-# Author: Mate Kukri
+# License for this file:
+#
+# Copyright (C) Mate Kukri, 2021
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 import binascii
